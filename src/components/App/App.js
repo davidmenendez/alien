8d7ccaed5c4313d6bb738a94b6afaaf39036bd45
David Menendez <dmenend@us.ibm.com>
2020-11-24 12:27:51 -0600

    fix: initial folder cleanup

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ALIEN</h1>
+      <h2>Welcome to the battle!</h2>
+    </div>
+  );
+}
+
+export default App;
