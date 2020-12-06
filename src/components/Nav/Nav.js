4d451da08935d66d3b9e19d2306fc28ad69fa3d4
David Menendez <dmenend@us.ibm.com>
2020-12-05 22:23:28 -0600

    feat: mobile nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   NavLink
 } from 'react-router-dom';
@@ -10,17 +10,38 @@ import './Nav.scss';
 const Nav = () => {
   const history = useHistory();
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
   const links = ['home', 'search', 'arena', 'bank'];
+
   const logoutHandler = e => {
     e.preventDefault();
     localStorage.removeItem('alienToken');
     dispatch(logout());
     history.push('/');
   };
+
+  const navToggleHandler = () => {
+    setOpen(!open);
+  };
+
+  const useNavToggle = window.innerWidth < 576;
+  const navClasses = ['nav'];
+  if (useNavToggle) {
+    if (open) navClasses.push('nav--open');
+    else navClasses.push('nav--closed');
+  }
+
   return (
-    <nav className="nav">
+    <nav className={navClasses.join(' ')}>
       <div className="container">
-        <h1>ALIEN</h1>
+        <div className="nav-head">
+          <h1>ALIEN</h1>
+          <div className="nav-toggle" onClick={navToggleHandler}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
         <div className="nav-links">
           {links.map(link => (
             <NavLink
