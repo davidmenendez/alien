7d1f1d1dbb9a9ed006b36d12df68becf866831f7
David Menendez <dmenend@us.ibm.com>
2021-09-23 16:39:32 -0500

    chore: update packages

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const dbUrl = process.env.DB_HOST;
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
